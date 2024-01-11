fda9906f0427b204f25470c9f37847b92210b2a8
Sw1ndlers <lisina2023@icloud.com>
2024-01-10 22:00:48 -0800

    Add open graph to recipe links

--- src/app/recipe/[...recipe]/page.tsx
+++ src/app/recipe/[...recipe]/page.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-"use client";
 
 import ErrorPage from "@components/Error";
-import { Err, Ok, Result } from "@/types/Results";
+import { Err, Ok, Result, ForceUnwrap } from "@/types/Results";
 import { RecipeInfo, RecipeInstruction } from "@/types/RecipeInfo";
 import { RatingStars } from "@components/RatingStars";
 import useSWR from "swr";
@@ -10,36 +9,51 @@ import { formatDate, formatDuration } from "@/functions/utils";
 import { Spinner } from "@components/Loader";
 import CenterLayout from "@components/layouts/Center";
 import { RawText } from "@components/RawText";
+import type { Metadata, ResolvingMetadata } from "next";
+import { getUrlInfo } from "./request";
+import { Recipe } from "@/types/Recipes";
+import { useState } from "react";
 
-function getUrlInfo({
-	recipeParams,
+// let recipeInfoCache: Result<RecipeInfo, string> | undefined = undefined;
+
+export async function generateMetadata({
+	params,
 }: {
-	recipeParams: [number, string];
-}): Result<
-	{
-		recipeId: number;
-		recipeRawTitle: string;
-	},
-	JSX.Element
-> {
-	let recipeId = recipeParams[0];
-	let recipeRawTitle = recipeParams[1];
+	params: { recipe: [number, string] };
+}): Promise<Metadata> {
+	const fetchResult = await getRecipeInfo({ params });
 
-	if (!recipeRawTitle) {
-		return Err(<ErrorPage caption="No recipe title found" />);
-	}
+    if (!fetchResult.ok) {
+        return {
+            title: "Error",
+            description: "Error",
+        };
+    }
 
-	if (!recipeId) {
-		return Err(<ErrorPage caption="No recipe id found" />);
-	}
+    const recipeResult = fetchResult.value;
 
-	if (isNaN(recipeId)) {
-		return Err(<ErrorPage caption={"Recipe ID is not a number"} />);
-	}
+    if (!recipeResult.ok) {
+        return {
+            title: "Error",
+            description: "Error",
+        };
+    }
 
-	recipeId = parseInt(recipeId as any);
+    const recipeInfo: RecipeInfo = (recipeResult.value as any)[0];
 
-	return Ok({ recipeId, recipeRawTitle });
+	return {
+		title: recipeInfo.headline,
+		description: recipeInfo.description,
+        openGraph: {
+            title: recipeInfo.headline,
+            description: recipeInfo.description,
+            images: [
+                {
+                    url: recipeInfo.image.url,
+                },
+            ],
+        },
+	};
 }
 
 function InfoBox({
@@ -106,47 +120,45 @@ function HowToStep({ instruction }: { instruction: RecipeInstruction }) {
 	);
 }
 
-export default function RecipePage({
+export async function getRecipeInfo(context: {
+	params: { recipe: any };
+}): Promise<Result<Result<Recipe, string>, JSX.Element>> {
+	const apiUrl = process.env.API_URL;
+	const recipeParams = context.params.recipe;
+	const urlInfo = getUrlInfo({ recipeParams });
+
+	if (!urlInfo.ok) {
+		Err(urlInfo.error);
+	}
+
+	const { recipeId, recipeRawTitle } = ForceUnwrap(urlInfo);
+
+	const response = await fetch(
+		`https://${apiUrl}/recipe/${recipeId}/${recipeRawTitle}`,
+	);
+
+	const data: Result<Recipe, string> = await response.json();
+	return Ok(data);
+}
+
+export default async function RecipePage({
 	params,
 }: {
 	params: { recipe: [number, string] };
 }) {
-	const apiUrl = process.env.API_URL;
-	const recipeParams = params.recipe;
-	const urlInfo = getUrlInfo({ recipeParams });
+	const fetchResult = await getRecipeInfo({ params });
 
-	if (!urlInfo.ok) {
-		return urlInfo.error;
+	if (!fetchResult.ok) {
+		return fetchResult.error;
 	}
 
-	const { recipeId, recipeRawTitle } = urlInfo.value;
-	const fetcher = (...args: [RequestInfo, RequestInit?]) =>
-		fetch(...args).then((res) => res.json());
+	const recipeResult = fetchResult.value;
 
-	const {
-		data,
-		error,
-		isLoading,
-	}: {
-		data: Result<RecipeInfo, string>;
-		error: any;
-		isLoading: any;
-	} = useSWR(
-		`https://${apiUrl}/recipe/${recipeId}/${recipeRawTitle}`,
-		fetcher,
-	);
+	if (!recipeResult.ok) {
+		return recipeResult.error;
+	}
 
-	if (error) return <ErrorPage caption={error} />;
-	if (isLoading)
-		return (
-			<CenterLayout>
-				<Spinner size={80} borderSize={7} />
-			</CenterLayout>
-		);
-	if (!data.ok) return <ErrorPage caption={data.error} />;
-
-	const recipeInfo: RecipeInfo = (data.value as any)[0];
-    console.log(recipeInfo);
+	const recipeInfo: RecipeInfo = (recipeResult.value as any)[0];
 
 	return (
 		<div className="min-h-screen max-w-[650px] self-center p-10 text-center text-dark-0">
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-export function Spinner({ size = 50, borderSize = 5 }) {
+export function Spinner({ size = 50, borderSize = 5, hidden = false }) {
 	return (
 		<div
 			className={`
                 animate-spin rounded-full 
                 border-white border-b-transparent
+                ${hidden ? "hidden" : "block"}
             `}
             style={{
                 width: size,
